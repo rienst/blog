cca69e890fbd3a5e2fcb3dc43364e240d1e88788
Rien Stenekes <r.stenekes@tfe.nl>
2025-03-13 10:50:53 +0100

    add homepage like to avatar

--- src/app/avatar.tsx
+++ src/app/avatar.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import rien from '@/app/assets/rien.jpg'
 import { info } from '@/info'
+import Link from 'next/link'
 
 export function Avatar() {
   return (
-    <Image
-      className="rounded-full shrink-0"
-      alt={info.name}
-      src={rien}
-      width="64"
-      height="64"
-    />
+    <Link href="/">
+      <Image
+        className="rounded-full shrink-0"
+        alt={info.name}
+        src={rien}
+        width="64"
+        height="64"
+      />
+    </Link>
   )
 }
