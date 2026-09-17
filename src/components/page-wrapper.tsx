c196d0d92c79bb95412705b20f136635997cfd2a
rienst <rienst@users.noreply.github.com>
2026-09-17 17:00:20 +0200

    styling

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 export function PageWrapper(props: React.PropsWithChildren) {
   return (
-    <div className="py-8 px-4 max-w-4xl mx-auto md:py-16">
+    <div className="py-8 px-4 max-w-4xl mx-auto sm:px-8 md:py-16">
       <header className="pb-8 md:pb-16">
         <Link
           to="/"
